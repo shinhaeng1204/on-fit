98412e64597a3b68cbd8b8107ae58ff634b2394b
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2025-10-31 17:24:58 +0900

    카드 컴포넌트 수정 중

---
+++ on-fit-next/src/components/common/Box.tsx
@@ -0,0 +1,15 @@
+// components/common/Box.tsx
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function Box({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "rounded-xl border border-border bg-card shadow-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+}
