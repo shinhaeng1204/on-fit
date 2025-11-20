8f70fb653812f264d6280b338b44bf7a354c3216
박총명 <qkrri56@naver.com>
2025-11-20 11:26:17 +0900

    feat : 채팅방 수정 및 헤더 레이아웃 수정

--- on-fit-next/src/components/main/FitCard.tsx
+++ on-fit-next/src/components/main/FitCard.tsx
@@ -28,9 +28,9 @@ export default function FitCard({
             <DumbbellIcon className="h-5 text-primary" />
           </div>
           <div className="flex flex-col">
-            <StatusBadge variant="success" className="absolute right-5">
+            <RecruitStatus type={status} text={status} className="absolute right-5">
               {status}
-            </StatusBadge>
+            </RecruitStatus>
             <h3 className="font-semibold text-lg group-hover:text-primary transition-colors">
               {title}
             </h3>
