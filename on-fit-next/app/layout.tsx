497d60493ce82d025f4580fd408be4875b0c5d3f
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2026-02-25 23:15:53 +0900

    카카오 지도 로드 오류 수정3

--- on-fit-next/app/layout.tsx
+++ on-fit-next/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <Script
-          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services`}
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services&autoload=false`}
           strategy="beforeInteractive"
         />
       </head>
