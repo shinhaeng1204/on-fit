ae888fb94fb87670a00b4e69174c59a460706866
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2026-02-25 22:59:29 +0900

    카카오 지도 로드 오류 수정

--- on-fit-next/app/layout.tsx
+++ on-fit-next/app/layout.tsx
@@ -1,28 +1,56 @@
-//layout.tsx;
+// //layout.tsx;
+// import './globals.css'
+// import { NotificationProvider } from "@/components/provider/NotificationProvider";
+// import Providers from "@/components/provider/Provider";
+// import Script from 'next/script';
+
+// export default async function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode
+// }>) {
+
+//   return (
+//     <html lang="ko">
+//       <body>
+//       <Providers>
+//         <NotificationProvider>
+//           {children}
+          
+//         </NotificationProvider>
+//       </Providers>
+//       <Script
+//           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services&autoload=false`}
+//           strategy="beforeInteractive"
+//         />
+//       </body>
+//     </html>
+//   )
+// }
 import './globals.css'
 import { NotificationProvider } from "@/components/provider/NotificationProvider";
 import Providers from "@/components/provider/Provider";
 import Script from 'next/script';
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
-
+}) {
   return (
     <html lang="ko">
-      <body>
-      <Providers>
-        <NotificationProvider>
-          {children}
-          
-        </NotificationProvider>
-      </Providers>
-      <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services&autoload=false`}
+      <head>
+        <Script
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services&autoload=false`}
           strategy="beforeInteractive"
         />
+      </head>
+      <body>
+        <Providers>
+          <NotificationProvider>
+            {children}
+          </NotificationProvider>
+        </Providers>
       </body>
     </html>
   )
